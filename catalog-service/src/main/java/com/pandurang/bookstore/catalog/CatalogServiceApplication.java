883e281e693fa801405faae0b6323c8c52473ad0
Pandurang Mane <pandurangmane654@gmail.com>
2024-04-24 14:19:25 +0530

    Workflow testing

--- catalog-service/src/main/java/com/pandurang/bookstore/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/pandurang/bookstore/catalog/CatalogServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
+        System.out.println("Workflow testing");
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
 }
