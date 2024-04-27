41231cf86ed9730bea50d871603913c943120da5
Pandurang Mane <pandurangmane654@gmail.com>
2024-04-27 09:28:45 +0530

    Github actions testing

--- catalog-service/src/main/java/com/pandurang/bookstore/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/pandurang/bookstore/catalog/CatalogServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
+        System.out.println("Github actions testing");
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
 }
