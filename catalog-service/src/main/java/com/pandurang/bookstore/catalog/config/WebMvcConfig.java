2ac5ea3073bf034bdd332e1ff1cd6c98101a8fe2
Pandurang Mane <pandurangmane654@gmail.com>
2024-05-01 19:03:44 +0530

    API Gateway service developed

---
+++ catalog-service/src/main/java/com/pandurang/bookstore/catalog/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.pandurang.bookstore.catalog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOriginPatterns("*")
+                .allowCredentials(false);
+    }
+
+}
---
+++ order-service/src/main/java/com/pandurang/orders/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.pandurang.orders.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOriginPatterns("*")
+                .allowCredentials(false);
+    }
+
+}
